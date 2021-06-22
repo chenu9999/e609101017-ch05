344fc4a0289c1b341140063857cc0ccc2d02eaac
rain <chenu9999@gmail.com>
2021-06-22 11:59:25 +0800

    c5-10定義printConstructionCost函數

--- SimVillage.kt
+++ SimVillage.kt
@@ -4,13 +4,15 @@ fun main() {
         println("新建$numBuildings" + "棟房屋")
         "歡迎來到SimVillage，$playerName! (copyright $currentYear)"
     }
-    /* 呼叫runSimulation("Guyal", greetingFunction) 將val greetingFunction的內容{ playerName: String, numBuildings: Int ->}
-    直接放入runSimulation中為參數 再將()往前移 只放入fun以外的String參數
-    參考課本5-9*/
 }
 
 inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String)
 {
     val numBuildings = (1..3).shuffled().last()
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
